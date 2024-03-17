d6d9798e0b0f4b688ed101df1f0e7880438c972c
yxz <yxzbox@gmail.com>
2024-03-17 15:20:38 -0400

    fast commit

--- repeat.go
+++ repeat.go
@@ -1,5 +1,7 @@
 package times
 
+import "time"
+
 //go:generate stringer -type Repeat
 type Repeat int
 
@@ -20,4 +22,27 @@ func (r Repeat) IsValid() bool {
 	default:
 		return false
 	}
+}
+
+func RepeatSchedule(repeat Repeat, initialStart time.Time, before time.Time) []time.Time {
+	var res []time.Time
+	t := initialStart
+	for t.Before(before) {
+		res = append(res, t)
+		switch repeat {
+		case Daily:
+			t = t.AddDate(0, 0, 1)
+		case Weekly:
+			t = t.AddDate(0, 0, 7)
+		case Monthly:
+			t = t.AddDate(0, 1, 0)
+		case Quarterly:
+			t = t.AddDate(0, 3, 0)
+		case Yearly:
+			t = t.AddDate(1, 0, 0)
+		default:
+			return res
+		}
+	}
+	return res
 }
